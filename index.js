2fc601ec5ce4d3245925b8f737db97512e174b05
Dil98 <44581944+Dil98@users.noreply.github.com>
2022-12-08 19:25:03 -0500

    first file

---
+++ index.js
@@ -0,0 +1,11 @@
+const express  = require('express');
+const app = express()
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/home',(req, res) => {
+  res.render('/views/home')
+})
+
+app.listen(4000)
