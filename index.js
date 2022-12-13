9b55d88315ee0033652ccae192446fac4b914d27
Tara Khanal <tarakh99@gmail.com>
2022-12-12 21:03:55 -0500

    Added a bootstrap template

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const app = express()
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get('/home',(req, res) => {
-  res.render('/views/home');
-})
+  res.render('home');
+});
 
-app.listen(4000);
+app.listen(4000, () => {
+  console.log("Server listening on port 4000");
+});
