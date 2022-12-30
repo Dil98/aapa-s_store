b9b61118a500b4d8e21ab2fa0fdc0f302e4706a9
Tara Khanal <tarakh99@gmail.com>
2022-12-30 02:52:28 -0500

    Automate the products instead of hard typing them in the html template. Later, pull these products from a database

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,8 @@
 const loggedIn = require('../misc/loggedIn');
+const products = require('../misc/products');
 
 const homeController = async (req, res) => {
-    res.render('home', {loggedIn: loggedIn.get(req.sessionID)});
+    res.render('home', {loggedIn: loggedIn.get(req.sessionID), products: products});
 }
 
 module.exports = {homeController}
