a0e1da4d33cc1d31a7250f30f744dfbacb1d36c5
Tara Khanal <tarakh99@gmail.com>
2022-12-31 13:55:26 -0500

    Show error page when user tries to go to the logout endpoint if user is not signed in

--- controllers/logout.js
+++ controllers/logout.js
@@ -1,9 +1,14 @@
 const loggedInUser = require('../misc/loggedIn');
 
 const getLogoutController = (req, res) => {
-    loggedInUser.delete(req.sessionID);
-    console.log("Appa has been logged out!");
-    res.redirect('/admin');
+    if(loggedInUser.get(req.sessionID)) {
+        loggedInUser.delete(req.sessionID);
+        console.log("Appa has been logged out!");
+        res.redirect('/admin');
+    } else {
+        res.redirect('/error');
+    }
+    
 }
 
 module.exports = {getLogoutController}
